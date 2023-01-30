d418bbca1de821f5600fc147b4d1a34ef1041c77
Ksubi 1k <xksubi1x@gmail.com>
2023-01-29 18:07:15 -0600

    updated code

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+// Connect to table we made in 'Models' folder
+const db = require('./models')
+
+// Post router
+
+const postRouter = require('./routes/PostsRoute')
+
+app.use('/posts', postRouter)
+
+// Comments Router
+
+const commentsRouter = require('./routes/Comments')
+
+app.use('/comments', commentsRouter)
+
+// start server
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log('Listening on port 3001')
+    })
+})
+
